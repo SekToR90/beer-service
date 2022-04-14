05cd74387d327e5adcb4a134bb1faed942c950ed
SekTor90 <ustimencko.nickita@yandex.ru>
2022-04-14 03:07:12 +0300

    fix: eslint label err

--- src/blocks/InputData.vue
+++ src/blocks/InputData.vue
@@ -1,14 +1,9 @@
 <template>
   <md-field>
+    <!--eslint-disable-next-line-->
     <label :for="`${id}_input`"
-      >{{ name }}:
-      <input
-        class="input"
-        type="text"
-        :id="`${id}_input`"
-        :value="data"
-        readonly
-      />
+      >{{ name }}
+      <md-input type="text" :id="`${id}_input`" :value="data" readonly />
     </label>
   </md-field>
 </template>
@@ -23,14 +18,3 @@ export default {
   },
 };
 </script>
-
-<style scoped lang="scss">
-.input {
-  padding: 0;
-  border: none;
-  background: none;
-  font-family: inherit;
-  font-size: 16px;
-  line-height: 32px;
-}
-</style>
